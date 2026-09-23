f2f27aef46e6c10b626d306cd3d6dcc9b35b7d3c
Dinkar Tiwari <dinkartiwari97@gmail.com>
2026-09-23 13:05:17 +0530

    Updated the db connection format

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,13 @@ const {Pool} = require("pg");
 require("dotenv").config({quite: true});
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+
+    connectionString: process.env.DB_URL,
 });
 
 pool.connect((error, client, release) => {
